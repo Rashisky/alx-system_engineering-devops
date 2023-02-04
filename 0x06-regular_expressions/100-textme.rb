4c1b917098b7311b5b934e0739e76dacfa86ea1f
Rashisky <rashisky007@gmail.com>
2023-02-04 15:47:26 +0100

    Regular Expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -5,6 +5,7 @@ File.open("text_messages.log", "r") do |file|
     line1 =  i.scan(/(?<=from:).*(?=\[flags)/).join("")
     line1 = line1.scan(/\+?\b(?!to|\W)\w+/).join(",")
     line2 = i.scan(/(?<=flags:).*(?=\] \[msg)/).join(",")
+    puts i
     puts line1 << "," << line2
     end
 end
