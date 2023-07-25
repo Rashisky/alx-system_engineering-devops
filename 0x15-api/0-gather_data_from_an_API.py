9773eb0b2d60d60b0c6075e2a0f366535fe49a07
Rashisky <Rashisky007@gmail.com>
2023-07-25 14:59:54 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """Gather data from an API"""
 
-from sys import argv
-import requests
+if __name__ == '__main__':
+    from sys import argv
+    import requests
 
-url = 'https://jsonplaceholder.typicode.com/'
-users = requests.get(url + f'users/{argv[1]}').json()
-todos = requests.get(url + f'todos?userId={argv[1]}').json()
+    url = 'https://jsonplaceholder.typicode.com/'
+    users = requests.get(url + f'users/{argv[1]}').json()
+    todos = requests.get(url + f'todos?userId={argv[1]}').json()
 
-task_completed = [task['title'] for task in todos if task['completed']]
+    task_completed = [task.get('title', '') for task in todos
+                      if task.get('completed', '')]
 
-print(f"Employee {users['name']} is done with tasks\
-      ({len(task_completed)}/{len(todos)}):")
-[print(f'\t {task}') for task in task_completed]
+    print(f"Employee {users.get('name', '')} is done with tasks\
+        ({len(task_completed)}/{len(todos)}):")
+    [print(f'\t {task}') for task in task_completed]
