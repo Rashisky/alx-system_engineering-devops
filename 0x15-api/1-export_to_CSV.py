43c21391f0aa6ef1c3ac955b5cb363e49a351747
Rashisky <Rashisky007@gmail.com>
2023-07-26 00:18:12 +0100

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,9 +9,6 @@ if __name__ == '__main__':
     user_name = requests.get(url + f'users/{argv[1]}').json().get('username')
     todos = requests.get(url + f'todos?userId={argv[1]}').json()
 
-    task_completed = [task.get('title') for task in todos
-                      if task.get('completed') is True]
-
     with open(f'{argv[1]}.csv', 'w', encoding='utf-8') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         [writer.writerow([argv[1], user_name, r.get('completed'),
